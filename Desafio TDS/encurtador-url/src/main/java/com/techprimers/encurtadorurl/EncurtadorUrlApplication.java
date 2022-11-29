854b3f17a581e8af5c284ce86c23dd3fbfa4e6f8
Vinicius Portugal <57232455+ViniciusPortugal-dev@users.noreply.github.com>
2022-11-29 10:07:38 -0300

    Add files via upload

---
+++ Desafio TDS/encurtador-url/src/main/java/com/techprimers/encurtadorurl/EncurtadorUrlApplication.java
@@ -0,0 +1,12 @@
+package com.techprimers.encurtadorurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUrlApplication.class, args);
+	}
+}
